3fbe36d2747e97f1538c86af5ec3bde21af6d7ff
Gabriel Lopes <gabrielrrlopes@gmail.com>
2021-03-19 23:06:11 -0300

    Developed usecase to handle 'readUF'

---
+++ src/usecases/readUF/readUF.ts
@@ -0,0 +1,18 @@
+import { Uf } from "@entities/Uf";
+import { ReadUFUseCase } from "./readUFUseCase";
+
+class ReadUF {
+  constructor(private readUFUsecase: ReadUFUseCase) {}
+
+  async listStateByUf(uf: string): Promise<Uf> {
+    const state = await this.readUFUsecase.listStateByUf(uf);
+    return state;
+  }
+
+  async listAllStates(): Promise<Uf[]> {
+    const states = await this.readUFUsecase.listAllStates();
+    return states;
+  }
+}
+
+export { ReadUF };
---
+++ src/usecases/readUF/readUFUseCase.ts
@@ -0,0 +1,16 @@
+import { Uf } from "@entities/Uf";
+import { IUFStore } from "@infraS/IUBrazilFstore";
+
+export class ReadUFUseCase {
+  constructor(private ufStore: IUFStore) {}
+
+  async listStateByUf(uf: string): Promise<Uf> {
+    const _uf = uf.toUpperCase()
+    return await this.ufStore.findStateByUF(_uf);
+  }
+
+  async listAllStates(): Promise<Uf[]> {
+    const ufs = await this.ufStore.findAllUF();
+    return ufs;
+  }
+}
